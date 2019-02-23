148b711cbc1b5b3893d66d01fd317f2a981ae5c4
DoisLucas <pedrolucas499@gmail.com>
2019-02-23 12:47:32 -0300

    Novas funcionalidades

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -9,7 +9,9 @@ export default class Main extends Component {
 
     state = {
         countries: [],
-        loading: true
+        continentes: ['África', 'América', 'Ásia', 'Europa', 'Oceania'],
+        loading: true,
+        escolhido: 'África'
     }
 
     componentDidMount() {
@@ -24,22 +26,42 @@ export default class Main extends Component {
 
     render() {
 
-        const { countries, loading } = this.state
+        const { countries, loading, continentes, escolhido } = this.state
 
         if (loading) {
             return <Loading />
         } else {
             return (
-                <div className="country-list">
-                    {countries.map(contry => (
-                        <article key={contry.id}>
-                            <strong>{contry.nome}</strong>
-                            <p>{contry.capital}</p>
-                            <p>{contry.populacao}</p>
-                            <Link to={`/details/${contry.nome}`}>Detalhes</Link>
-                        </article>
-                    ))}
+                <div>
+                    <div className="country-list">
+
+                        <div className="botoes">
+
+                            {continentes.map(value => {
+
+                                //Por onclick aqui
+                                if (value === escolhido) {
+                                    return <a href="#" className="active">{value}</a>
+                                } else {
+                                    return <a href="#">{value}</a>
+                                }
+
+                            })}
+
+                        </div>
+
+                        {countries.map(contry => (
+                            <article key={contry.id}>
+                                <strong>{contry.nome}</strong>
+                                <p>{contry.capital}</p>
+                                <p>{contry.populacao}</p>
+                                <Link to={`/details/${contry.nome}`}>Detalhes</Link>
+                            </article>
+                        ))}
+                        
+                    </div>
                 </div>
+
             )
         }
     }
